aff5a070916253aabc15984b5304f9e482e9e1c6
xkapusta <xkapusta@stuba.sk>
2025-05-04 04:31:28 +0200

    Adding the filtration by segmenting the planes

--- pvso4.py
+++ pvso4.py
@@ -1,12 +1,21 @@
 import numpy as np
 import open3d as o3d
+import copy
+import random
 
-def main(file):
-    # use open3d
-    use_o3d(file)
+def generate_random_colors(n):
+    colors = []
+    for _ in range(n):
+        color = np.random.rand(3)
+        colors.append(color)
+    return colors
 
+def main(file,colors):
+    data = use_o3d(file,colors)
 
-def use_o3d(file):
+    cleanUp(data)
+
+def use_o3d(file,colors):
     pcd = o3d.geometry.PointCloud()
     #pcd.points = o3d.utility.Vector3dVector(pts)
 
@@ -16,24 +25,58 @@ def use_o3d(file):
     #Converting pcd.points to array
     pointCloudPoints = np.array(pcd.points)
 
-
     #Get rid of NaN values
     pointCloudPointMasking = np.isfinite(pointCloudPoints).all(axis=1)
     pointCloudPointFiltered = pointCloudPoints[pointCloudPointMasking]
-
 
     pointCloudPoints = o3d.geometry.PointCloud()
     pointCloudPoints.points = o3d.utility.Vector3dVector(pointCloudPointFiltered) #XYZ coordinates
 
     #Add the colors
-    pointCloudPointsColors= np.asarray(pcd.colors)
-    pointCloudPointsColorsFiltered = pointCloudPointsColors[pointCloudPointMasking]
+    if(colors):pointCloudPointsColors= np.asarray(pcd.colors)
+    if(colors):pointCloudPointsColorsFiltered = pointCloudPointsColors[pointCloudPointMasking]
 
-    #Here are the colors added to the whole
-    pointCloudPoints.colors=o3d.utility.Vector3dVector(pointCloudPointsColorsFiltered) #Colors
+    if(colors):pointCloudPoints.colors=o3d.utility.Vector3dVector(pointCloudPointsColorsFiltered) #Colors
 
     #Show the scan
-    o3d.visualization.draw_geometries([pointCloudPoints])
+    #o3d.visualization.draw_geometries([pointCloudPoints])
+
+    return(pointCloudPoints)
+
+def cleanUp(pointCloud):
+    pcd = pointCloud
+
+    number_of_planes = 10
+    planes =[]
+    colors = generate_random_colors(number_of_planes)
+    planeMemory = o3d.geometry.PointCloud()
+    planes_combined = o3d.geometry.PointCloud()
+
+    #Detect multiply planes
+    for i in range(number_of_planes):
+        plane_model, plane = pcd.segment_plane(0.01, 3, 1000)
+
+        plane_points = pcd.select_by_index(plane, invert=False)
+
+        planeMemory.points = plane_points.points
+        planeMemory.colors = plane_points.colors
+        #Color unifi
+        #plane_points.paint_uniform_color(colors[i % len(colors)])
+
+        planes.append(plane_points)
+
+        # Remove inliers for next iteration
+        pcd = pcd.select_by_index(plane, invert=True)
+
+        planes_combined += planeMemory
 
 
-main('output.pcd')
+    #pcd.paint_uniform_color([0.5, 0.5, 0.5])  # Gray for remaining points
+
+    o3d.visualization.draw_geometries([planeMemory]) #Display planes
+
+    #o3d.visualization.draw_geometries([pcd])
+
+
+
+main('output_big.pcd',False)
